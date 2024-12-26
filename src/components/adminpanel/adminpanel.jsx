b7a4b6ea8e4dc46f1c8721f686ca87f435ef10a2
CarlosGuedea <57150568+CarlosGuedea@users.noreply.github.com>
2024-12-26 14:47:35 -0600

    Panel de Administración V1

--- src/components/adminpanel/adminpanel.jsx
+++ src/components/adminpanel/adminpanel.jsx
@@ -14,60 +14,69 @@ function Adminpanel() {
     <h5>Administración de trámites, requisitios, áreas y puestos</h5>
     </div>
    
-    <div className="container">
+    <div className="fluid-container">
 
   <section className="section">
-    <h2>Sección 1</h2>
-    <div className="row">
-      <div className="col-md-3">
-        <div className="box-tramites">
-          <a href="">
+
+    <div className="row flex-row">
+      <div className="col-md-3 flex-item justify-content-center">
+      
+      <a href="">
+        <div className="box box-tramites">
           <img src="icons/tramites.png" alt="tramites" className="img-tramites"/>
           <h4>Trámites</h4>
-          <h6>Administración de trámites, requisitos, áreas y puestos</h6>
-          </a>
+          <h6 className="title">Administración de trámites, requisitos, áreas y puestos</h6>
         </div>
+        </a>
+
       </div>
       <div className="col-md-3">
-        <div className="box">Columna 2</div>
+      
+      <a href="">
+        <div className="box box-usuarios">
+            <img src="icons/usuarios.png" alt="usuarios" className="img-usuarios"/>
+            <h4>Panel de Módulos y Usuarios</h4>
+          <h6>Administración de módulos y usuarios</h6>
+        </div>
+        </a>
+
       </div>
       <div className="col-md-3">
-        <div className="box">Columna 3</div>
+      <a href="">
+        <div className="box box-empleados">
+            <img src="icons/empleados.png" alt="areas" className="img-empleados"/>
+            <h4>Módulo de empleados</h4>
+          <h6>Administración para la lista de empleados</h6>
+        </div>
+        </a>
+    
       </div>
       <div className="col-md-3">
-        <div className="box">Columna 4</div>
+      <a href="">
+        <div className="box box-micrositios">
+          
+            <img src="icons/micrositios.png" alt="" srcset="" className="img-micrositios"/>
+            <h4>Micrositios</h4>
+          <h6>Administración para Micrositios</h6>
+          
+        </div>
+        </a>
       </div>
+
+      <div className="col-md-3">
+      <a href="">
+        <div className="box box-oficialia">
+            <img src="icons/oficialia.png" alt="oficialia" srcset="" className="img-oficialia"/>
+            <h4>Oficicialia de Partes</h4>
+          <h6>Administración de oficios y documentos</h6>
+        </div>
+        </a>
+      </div>
+
     </div>
   </section>
 
 
-  <section class="section">
-    <h2>Sección 2</h2>
-    <div class="row">
-      <div class="col-md-4">
-        <div class="box">Columna 1</div>
-      </div>
-      <div class="col-md-4">
-        <div class="box">Columna 2</div>
-      </div>
-      <div class="col-md-4">
-        <div class="box">Columna 3</div>
-      </div>
-    </div>
-  </section>
-
-
-  <section class="section">
-    <h2>Sección 3</h2>
-    <div class="row">
-      <div class="col-md-6">
-        <div class="box">Columna 1</div>
-      </div>
-      <div class="col-md-6">
-        <div class="box">Columna 2</div>
-      </div>
-    </div>
-  </section>
 </div>
 
 
